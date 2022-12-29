52c952d099e28c6f31f6d4aa6ca01f04a93a74bf
tlukend <tina.lukenda@hotmail.com>
2022-12-29 14:20:03 +0100

    tried a new branch with only the class of a node which will create children nodes and the rest will be functions

--- main.py
+++ main.py
@@ -1,27 +1,70 @@
-# main is our compare
-
-#Variables: heuristic(heuristic) .. do we need this on???
-# further variables: grid gridStart();
-
-#functions: createGrids(Hamming); createGrids(Manhattan); compare (hamming & manhattan)
+# main is our compare?
 
 
 
+#class Node:
+
+#    def __init__(self, gValue, hValue):
+        #f(n) = g(n) + h(n) manhattan oder hamming
+
+
+ #   def createChild(self):
+        #g(n) = g(n) + 1
+
+#def solve_manhattan (start):
+    #current = smallest f(n)
+    # create children nodes (raise g(n)+1 and add h(n) of this very child-node)
+    # my thinking right now: easier to have a list full of all f(n)'s and
+    # in every round sort them and pick the smallest
+    # this would - in my eyes - make the most sense and always make sure we choose the lowest cost
+    # pick smallest
+    # if h(n) == 0 we are done, otherwise to recursive function -> repeat
+    # if solved, return goal array
+
+#def solve_hamming (start):
+    #current = smallest f(n)
+    #create children nodes (raise g(n)+1 and add h(n) of this very child-node)
+    # my thinking right now: easier to have a list full of all f(n)'s and
+    # in every round sort them and pick the smallest
+    # this would - in my eyes - make the most sense and always make sure we choose the lowest cost
+    # pick smallest
+    # if h(n) == 0 we are done, otherwise to recursive function -> repeat
+    # if solved, return goal array
+
+#def hamming (self, start, goal):
+#    temp = 0
+#    for i in range (0, 2):
+#        for j in range (0,2):
+#            if start[i][j] != goal[i][j] and start[i][j]!='_':
+#                temp += 1
+#    return temp
 
 
 
-# This is a sample Python script.
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#consider, that there must be a state of not going back
+#should we empty the queue after we have chosen a child node?
+# if h == 0 , we are done
+# do we want to print every state until we reach goal? i say yes
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+
+
+#def init ():
+    # initialize array
+    # checkIfSolvable
+    #f = g + h
+    #node
+
+
+
+
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('test')
+    print('Hello, World!')
+#print(node with smallest f(n) && h(n) 00 0)
+# also print
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
