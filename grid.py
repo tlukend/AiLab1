10589a156edabe3cd146be0c768be54b0f936afb
christinamoser <christina.moser1@stud.fh-campuswien.ac.at>
2023-01-02 17:36:02 +0100

    main - added print Goal grid - changed print_goal (first x and then y)

--- grid.py
+++ grid.py
@@ -1,35 +1,50 @@
-#inherits to start and goal (2 extra classes??? - wie wird dies in python gelöst?)
+# inherits to start and goal (2 extra classes??? - wie wird dies in python gelöst?)
 import random
 
-#variables: int i; array[3][3] ... kann sein dass für Befüllung noch ein array[9] benötigt wirdfun
 
-#functions: fill(array); print(array)
+# variables: int i; array[3][3] ... kann sein dass für Befüllung noch ein array[9] benötigt wirdfun
 
-#for i in range (0, 8):
+# functions: fill(array); print(array)
+
+# for i in range (0, 8):
 #    while (self.is_solved == true):
 
 
 def create_random_grid():
     array = [[], [], []]
-# a set numbers we are choosing from
+    # a set numbers we are choosing from
     numbers = list(range(0, 9))
-#iterate trough grid and pick random number from numbers, with remove it is guaranteed that the same number will
-#not occur
+    # iterate trough grid and pick random number from numbers, with remove it is guaranteed that the same number will
+    # not occur
     for x in range(0, 3):
         for y in range(0, 3):
             number = random.choice(numbers)
             numbers.remove(number)
             array[x].append(number)
+
+    while check_solvability(array):
+        array = create_random_grid()
+
+    print('out of while loop')
+
     return array
 
 
 def print_grid(array):
-    for y in range(0, 3):
-        for x in range(0, 3):
+    #Christina changed to first for x und seccond for y -> if we change it back also change main grid_goal and check_solvability
+    for x in range(0, 3):
+        for y in range(0, 3):
             print(str(array[x][y]) + " ", end="")
         print()
 
 
-#grid = create_random_grid()
-#print_grid(grid)
-#print(grid)
+def check_solvability(array):
+    # reference: https://www.youtube.com/watch?v=bhmCmbj9VAg and https://www.geeksforgeeks.org/check-instance-8-puzzle-solvable/
+    # 9! possible initial states but only half of these are solvable -> (9!)/2 initial states are solvable)
+    # check solvability with inversion count - even: solvable; odd: not solvable
+
+
+
+# grid = create_random_grid()
+# print_grid(grid)
+# print(grid)
--- main.py
+++ main.py
@@ -2,28 +2,30 @@
 # import numpy as np
 import grid
 
+
 def main():
     grid_goal = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+
+    print('Goal State: ')
+    grid.print_grid(grid_goal)
+    print('\n')
+
     grid_list = []
 
-    #list 100 arrays
-    for i in range(100):
+    # creates a list of 100 random grids
+    for i in range(1):
         grid_list.append(grid.create_random_grid())
 
-    #prints length and arrays
+    # prints length and arrays
     print(str(len(grid_list)) + ' grids are created: \n')
     for i in grid_list:
         grid.print_grid(i)
         print()
 
 
-
 if __name__ == "__main__":
     main()
 
-
-
-#puzzle = new puzzle()
-#solver = new solverAstar(heuristichamming)
-#solver.solve(puzzle)
-
+# puzzle = new puzzle()
+# solver = new solverAstar(heuristichamming)
+# solver.solve(puzzle)
