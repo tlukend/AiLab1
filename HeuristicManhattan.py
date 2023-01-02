92f7f418abec78e71fa1e8a8e2d28bb317450ef9
tlukend <tina.lukenda@hotmail.com>
2023-01-02 17:28:54 +0100

    manhattan idea

--- HeuristicManhattan.py
+++ HeuristicManhattan.py
@@ -1,11 +1,39 @@
-#inherits to manhattan and hemming
 from Heuristic import Heuristic
 
 
-#Variables: countSteps, runtime(count), memoryEffort(numberOfNodes)
 
-#class HeuristicManhattan(Heuristic):
-#    def calculate(self, node, start, goal):
-        # TODO: implement
+class HeuristicManhattan (Heuristic):
+    # Variables: countSteps, runtime(count), memoryEffort(numberOfNodes)
+    # stillNotDone
+
+    def findDistance(self, val, goal):
+        x1 = y1 = 0
+
+        for i in range (0, 2):
+            for j in range (0, 2):
+                if goal[i][j] == val:
+                    x1 = i
+                    y1 = j
+                    break
+
+        return x1, y1
+
+    def calculate (self, node, start, goal):
+        manhattanDistance = 0
+        countSteps = 0
+        x1 = x2 = y1 = y2 = 999
+        total_h = 0
+
+
+        for i in range (3):
+            for j in range (3):
+                x1,y1 = self.findDistance(start[i][j],goal)
+                x2,y2 = i,j
+                countSteps = abs(x1-x2) + abs(y1-y2)
+
+        return countSteps
+
+
+
 
 
