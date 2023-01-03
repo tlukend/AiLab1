2eaced084248fb82237cc72ad101be933571a435
Slavica <54640856+Slavica-lab@users.noreply.github.com>
2023-01-03 12:28:30 +0100

    range changed

--- HeuristicHamming.py
+++ HeuristicHamming.py
@@ -8,8 +8,8 @@ class HeuristicHamming(Heuristic):
     def calculate(self, node, start, goal):
         #this is the distance that the heuristic calculates
         hammingDistance = 0
-        for i in range(0, 2):
-            for j in range(0, 2):
+        for i in range(0, 3):
+            for j in range(0, 3):
                 if node.state[i][j] != goal[i][j] and node.state[i][j] != 0:
                     hammingDistance += 1
         return hammingDistance
