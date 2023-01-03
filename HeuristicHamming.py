a1d6cefadaad8cdbb8e09a0aa2388b0257126086
tlukend <tina.lukenda@hotmail.com>
2023-01-03 18:05:33 +0100

    manhattan done?

--- HeuristicHamming.py
+++ HeuristicHamming.py
@@ -1,13 +1,17 @@
 from Heuristic import Heuristic
 
 class HeuristicHamming(Heuristic):
-    def calculate(self, node, start, goal):
+    def calculate(self, node, start, goal): #start is unneccessary or not?
         #it counts the wrong states of the puzzle
+        # goes through the two-dimensional array
         hammingDistance = 0
         for i in range(0, 3):
             for j in range(0, 3):
+                # if current node state is not the same as the goal state or 0
                 if node.state[i][j] != goal[i][j] and node.state[i][j] != 0:
+                    # we add +1 for the hammingDistance, because it rises the number of misplaced tiles except 0. which is not a tile
                     hammingDistance += 1
-        return hammingDistance
+                    # return number of misplaced tiles as an integer
+                    return hammingDistance
 
 
--- HeuristicManhattan.py
+++ HeuristicManhattan.py
@@ -6,32 +6,28 @@ class HeuristicManhattan (Heuristic):
     # Variables: countSteps, runtime(count), memoryEffort(numberOfNodes)
     # stillNotDone
 
-    def findDistance(self, val, goal):
-        x1 = y1 = 0
 
-        for i in range (0, 3):
-            for j in range (0, 3):
-                if goal[i][j] == val:
+    def find_distance(self, current, goal):
+        x1 = y1 = 0
+        for i in range(0, 3):
+            for j in range(0, 3):
+                if goal[i][j] == current:
                     x1 = i
                     y1 = j
                     break
-
         return x1, y1
 
-    def calculate (self, node, start, goal):
-        manhattanDistance = 0
-        countSteps = 0
-        x1 = x2 = y1 = y2 = 999
+    def calculate(self, node, current, goal):
         total_h = 0
+        x1 = x2 = y1 = y2 = 0
+        for i in range(0, 3):
+            for j in range(0, 3):
+                x1, y1 = self.find_distance(current[i][j], goal)
+                x2, y2 = i, j
+                tilesteps = abs(x1-x2) + abs(y1-y2)
+                total_h += tilesteps
 
-
-        for i in range (3):
-            for j in range (3):
-                x1,y1 = self.findDistance(start[i][j],goal)
-                x2,y2 = i,j
-                countSteps = abs(x1-x2) + abs(y1-y2)
-
-        return countSteps
+        return total_h
 
 
 
