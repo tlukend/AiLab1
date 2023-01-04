1ca106a255811c37078043e4c168762ec8e71c8d
Slavica <54640856+Slavica-lab@users.noreply.github.com>
2023-01-04 22:50:27 +0100

    Puzzle Kommentar fertig

--- Puzzle.py
+++ Puzzle.py
@@ -1,3 +1,6 @@
+"""Puzzle class: knows two states. The randomly generated start_state and the designated grid_goal state """
+
+
 class Puzzle:
     def __init__(self, start_state, grid_goal):
         self.start_state = start_state
